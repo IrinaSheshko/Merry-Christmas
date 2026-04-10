05d07ae208b096e5418e3f81b97092dbdb044425
Irina Sheshko <irinasheshko1508@gmail.com>
2026-04-10 16:56:02 +0300

    fix: added missing diff variable

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function merryChristmas() {
 
 // ======== Таймер ========
 function christmasCountdown() {
-    const christmasDate = new Date("2026-12-25T00:00:00");
+    const christmasDate = new Date("December 25, 2026 00:00:00");
     const now = new Date();
     const diff = christmasDate - now;
 
